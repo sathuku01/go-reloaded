ca82d491fcf8c2d788acb5bbada1bae6864fcb01
sathuku <samuelnjiri@gmail.com>
2026-01-14 16:41:09 +0300

    feat: add package that will convert from hexadecimal to decimal and from binary to decimal

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,8 @@ import (
 	"bufio"
 	"log"
 	"strings"
-	hc "go_reloaded/pkg/capitalise"
+	sc "go_reloaded/pkg/capitalise"
+	hc "go_reloaded/pkg/baseConversion"
 )
 
 func main(){
@@ -40,22 +41,71 @@ func main(){
 	splitted := strings.Fields(text)
 	for i := 0; i < len(splitted); i++ {
 		if splitted[i] == "(cap)" {
-			splitted[i-1] = hc.CapsFirstLetter(splitted[i-1])
+			splitted[i-1] = sc.CapsFirstLetter(splitted[i-1])
 			splitted = append(splitted[:i], splitted[i+1:]...)
 			strings.Join(splitted, " ")
 		}else if splitted[i] == "(cap,"{
-			iter := hc.ExrtractNumFromString(splitted[i+1])
+			iter := sc.ExrtractNumFromString(splitted[i+1])
+			if iter > i{
+				log.Fatal(err)
+				return
+			}
 			for j := 1; j <= iter; j++ {
-				splitted[i-j] = hc.CapsFirstLetter(splitted[i-j])
+				splitted[i-j] = sc.CapsFirstLetter(splitted[i-j])
 			}
 			splitted = append(splitted[:i], splitted[i+2:]...)
 			strings.Join(splitted, " ")
 		}else if splitted[i] == "(up)"{
-
+			splitted[i-1] = sc.ToUpper(splitted[i-1])
+			splitted = append(splitted[:i], splitted[i+1:]...)
+			strings.Join(splitted, " ")
+		}else if splitted[i] == "(up,"{
+			iter := sc.ExrtractNumFromString(splitted[i+1])
+			if iter > i{
+				log.Fatal(err)
+				return
+			}
+			for j := 1; j <= iter; j++ {
+				splitted[i-j] = sc.ToUpper(splitted[i-j])
+			}
+			splitted = append(splitted[:i], splitted[i+2:]...)
+			strings.Join(splitted, " ")
+		}else if splitted[i] == "(low)"{
+			splitted[i-1] = sc.ToLower(splitted[i-1])
+			splitted = append(splitted[:i], splitted[i+1:]...)
+			strings.Join(splitted, " ")
+		}else if splitted[i] == "(low,"{
+			iter := sc.ExrtractNumFromString(splitted[i+1])
+			if iter > i{
+				log.Fatal(err)
+				return
+			}
+			for j := 1; j <= iter; j++ {
+				splitted[i-j] = sc.ToLower(splitted[i-j])
+			}
+			splitted = append(splitted[:i], splitted[i+2:]...)
+			strings.Join(splitted, " ")
+		}else if splitted[i] == "(hex)" && i > 0 {
+			splitted[i-1] = hc.HexTodec(splitted[i-1])
+			splitted = append(splitted[:i], splitted[i+1:]...)
+			strings.Join(splitted, " ")
+		}else if splitted[i] == "(bin)" && i > 0 {
+			splitted[i-1] = hc.BinaryToDecimal(splitted[i-1])
+			splitted = append(splitted[:i], splitted[i+1:]...)
+			strings.Join(splitted, " ")
 		}
 
 	}
+	
+	finalResult := ""
+	for i, char := range splitted {
+		if i == 0{
+			finalResult += char
+		}else {
+			finalResult += " " + char
+		}
+	}
 	fmt.Println(text)
 	fmt.Println()
-	fmt.Println(splitted)
+	fmt.Println(finalResult)
 }
