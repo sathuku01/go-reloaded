1c3445078124c65730d6cd780217acbb1ff09550
sathuku <samuelnjiri@gmail.com>
2026-01-14 16:43:02 +0300

    feat: add function that coverts from binary to decimal

---
+++ pkg/baseConversion/baseConv.go
@@ -0,0 +1,24 @@
+package go_reloaded
+
+import(
+	"strconv"
+)
+
+func HexTodec(strHex string) string {
+	hex, err := strconv.ParseInt(strHex, 16, 64)
+
+	if err != nil {
+		return "Error converting hex to dec."
+	}
+	return strconv.FormatInt(hex, 10)
+}
+
+// converts a binary string to decimal.
+func BinaryToDecimal(bin string) string {
+	dec := 0
+	for i := 0; i < len(bin); i++ {
+		dec = dec*2 + int(bin[i]-'0')
+
+	}
+	return strconv.Itoa(dec)
+}
