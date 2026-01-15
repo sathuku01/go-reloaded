4329cf7b733af96f5caae279600263415abdf5ba
sathuku <samuelnjiri96@gmail.com>
2026-01-15 10:43:45 +0300

    refactor: reduce bloat in the main.go file code, by moving the code to process. I call the code with a single line in main

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"bufio"
 	"log"
-	"strings"
-	sc "go_reloaded/pkg/capitalise"
-	hc "go_reloaded/pkg/baseConversion"
+	// "strings"
+	// sc "go_reloaded/pkg/capitalise"
+	// hc "go_reloaded/pkg/baseConversion"
+	pc "go_reloaded/process"
 )
 
 func main(){
@@ -38,74 +39,8 @@ func main(){
 		return
 	}
 
-	splitted := strings.Fields(text)
-	for i := 0; i < len(splitted); i++ {
-		if splitted[i] == "(cap)" {
-			splitted[i-1] = sc.CapsFirstLetter(splitted[i-1])
-			splitted = append(splitted[:i], splitted[i+1:]...)
-			strings.Join(splitted, " ")
-		}else if splitted[i] == "(cap,"{
-			iter := sc.ExrtractNumFromString(splitted[i+1])
-			if iter > i{
-				log.Fatal(err)
-				return
-			}
-			for j := 1; j <= iter; j++ {
-				splitted[i-j] = sc.CapsFirstLetter(splitted[i-j])
-			}
-			splitted = append(splitted[:i], splitted[i+2:]...)
-			strings.Join(splitted, " ")
-		}else if splitted[i] == "(up)"{
-			splitted[i-1] = sc.ToUpper(splitted[i-1])
-			splitted = append(splitted[:i], splitted[i+1:]...)
-			strings.Join(splitted, " ")
-		}else if splitted[i] == "(up,"{
-			iter := sc.ExrtractNumFromString(splitted[i+1])
-			if iter > i{
-				log.Fatal(err)
-				return
-			}
-			for j := 1; j <= iter; j++ {
-				splitted[i-j] = sc.ToUpper(splitted[i-j])
-			}
-			splitted = append(splitted[:i], splitted[i+2:]...)
-			strings.Join(splitted, " ")
-		}else if splitted[i] == "(low)"{
-			splitted[i-1] = sc.ToLower(splitted[i-1])
-			splitted = append(splitted[:i], splitted[i+1:]...)
-			strings.Join(splitted, " ")
-		}else if splitted[i] == "(low,"{
-			iter := sc.ExrtractNumFromString(splitted[i+1])
-			if iter > i{
-				log.Fatal(err)
-				return
-			}
-			for j := 1; j <= iter; j++ {
-				splitted[i-j] = sc.ToLower(splitted[i-j])
-			}
-			splitted = append(splitted[:i], splitted[i+2:]...)
-			strings.Join(splitted, " ")
-		}else if splitted[i] == "(hex)" && i > 0 {
-			splitted[i-1] = hc.HexTodec(splitted[i-1])
-			splitted = append(splitted[:i], splitted[i+1:]...)
-			strings.Join(splitted, " ")
-		}else if splitted[i] == "(bin)" && i > 0 {
-			splitted[i-1] = hc.BinaryToDecimal(splitted[i-1])
-			splitted = append(splitted[:i], splitted[i+1:]...)
-			strings.Join(splitted, " ")
-		}
-
-	}
-	
-	finalResult := ""
-	for i, char := range splitted {
-		if i == 0{
-			finalResult += char
-		}else {
-			finalResult += " " + char
-		}
-	}
 	fmt.Println(text)
-	fmt.Println()
-	fmt.Println(finalResult)
+	// fmt.Println(len(splitted))
+	// fmt.Println(splitted)
+	fmt.Println(pc.ProcessData(text))
 }
