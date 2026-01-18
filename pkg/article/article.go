dc57e54826898608c6724d6ddfd344903d1def66
sathuku <samuelnjiri96@gmail.com>
2026-01-18 13:57:48 +0300

    Refactor: enable function to recieve a slice of strings instead of a string

--- pkg/article/article.go
+++ pkg/article/article.go
@@ -1,12 +1,21 @@
 package go_reloaded
 
-func CheckForArticle(s string) string{
-	mapArticles := map[byte]struct{}{
-			'a' : {}, 'e' : {},	'i' : {}, 'o' : {}, 'u' : {}, 'h' : {},
-			'A' : {}, 'E' : {}, 'I' : {}, 'O' : {}, 'U' : {}, 'H' : {},
+func CheckForArticle(s []string) []string{
+	if len(s) < 2 {
+		return s
+	}
+	
+	mapArticles := map[string]struct{}{
+	"a": {}, "e": {}, "i": {}, "o": {}, "u": {}, "h": {},
+	"A": {}, "E": {}, "I": {}, "O": {}, "U": {}, "H": {},
+}
+		for i, char := range s {
+			if (char == "a" || char == "A" && s[i+1] == " ") {
+				if _, exists := mapArticles[s[i+2]]; exists {
+				char = "an"
 			}
-			if _, exists := mapArticles[s[0]]; exists {
-				s = "an"
-			}
-			return s
+		}
+	}
+	
+	return s
 }
